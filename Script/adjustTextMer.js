be37511c7c8366f42f68b64633d9871ab556d974
CodeMPLady <cathala.naemie@gmail.com>
2023-06-29 19:26:37 +0200

    changement de "plats tradi" à "plats classiques" (titres et classes/id)
    Co-authored-by: MPCodeur <MPCodeur@users.noreply.github.com>

---
+++ Script/adjustTextMer.js
@@ -0,0 +1,32 @@
+let activateur = true;
+
+function genererBaliseP() {
+    let nouvelElementP = document.createElement("p");
+    let parentElement = document.getElementById("textPres");
+    let largeurEcran = window.innerWidth;
+  
+    if (activateur) {
+        activateur = false;
+        if (largeurEcran < 770) {
+        nouvelElementP.textContent = "Nous allons présenter ici les bases qui sont nécessaires à la réalisation des recettes proposées sur ce site.";
+        } else {
+        nouvelElementP.textContent = "A COMPLETER";
+        }
+        nouvelElementP.setAttribute('id', 'pres');
+        parentElement.appendChild(nouvelElementP);
+        
+        let  createStyle =  document.createElement("style");
+        let style = `
+        #textPres {
+            width: 70%;
+          }
+        `
+        createStyle.innerText = style;
+        parentElement.appendChild(createStyle);
+    }
+}
+  
+window.onload = genererBaliseP;
+window.onresize = genererBaliseP;
+
+
